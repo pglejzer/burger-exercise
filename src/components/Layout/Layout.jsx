9689b43b00b90c1ae56042c20ebdb682b998a6ad
pjoter <p.glejzer@mdbootstrap.com>
2019-02-22 00:02:07 +0100

    Changed

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,13 +1,30 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Aux from '../../hoc/Aux';
 import classes from './Layout.module.css';
 import Toolbar from '../Navigation/Toolbar/Toolbar';
+import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 
-const layout = props => (
-  <Aux>
-    <Toolbar />
-    <main className={classes.Content}>{props.children}</main>
-  </Aux>
-);
+class Layout extends Component {
+  state = {
+    showSideDrawer: true,
+  };
 
-export default layout;
+  sideDrawerCloseHandler = () => {
+    this.setState({ showSideDrawer: false });
+  };
+
+  render() {
+    return (
+      <Aux>
+        <Toolbar />
+        <SideDrawer
+          open={this.state.showSideDrawer}
+          closed={this.sideDrawerCloseHandler}
+        />
+        <main className={classes.Content}>{this.props.children}</main>
+      </Aux>
+    );
+  }
+}
+
+export default Layout;
--- src/components/UI/Backdrop/Backdrop.jsx
+++ src/components/UI/Backdrop/Backdrop.jsx
@@ -3,7 +3,7 @@ import classes from './Backdrop.module.css';
 
 const backdrop = props =>
   props.show ? (
-    <div className={classes.Backdrop} onClick={props.clickedBackdrop} />
+    <div className={classes.Backdrop} onClick={props.clicked} />
   ) : null;
 
 export default backdrop;
