36573d9ce8203f2450edcfc1ab5690ecbc4380ef
pjoter <p.glejzer@mdbootstrap.com>
2019-02-21 16:49:50 +0100

    changed

--- src/components/Burger/OrderSummary/OrderSummary.jsx
+++ src/components/Burger/OrderSummary/OrderSummary.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import Aux from '../../../hoc/Aux';
+import classes from './OrderSummary.module.css';
+import Button from '../../UI/Button/Button';
 
 const orderSummary = props => {
   const ingredientSummary = Object.keys(props.ingredients).map(igKey => {
@@ -16,7 +18,18 @@ const orderSummary = props => {
       <h3>Your order</h3>
       <p>A delicious burger with the following ingredients</p>
       <ul>{ingredientSummary}</ul>
+      <p>
+        <strong>Total Price: {props.price.toFixed(2)}$</strong>
+      </p>
       <p>Contiune to Checkout?</p>
+      <div className={classes.OrderSummary}>
+        <Button btnType="Danger" clicked={props.purchaseCancelled}>
+          Cancel
+        </Button>
+        <Button btnType="Success" clicked={props.purchaseContinuted}>
+          Contiune
+        </Button>
+      </div>
     </Aux>
   );
 };
